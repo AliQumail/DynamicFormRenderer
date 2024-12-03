95b687ea865af9b1ef7fd686de148f6169d05c65
Ali Qumail <aliqumail8@gmail.com>
2024-12-03 19:36:29 +0500

    AppComponent Made Standalone

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './services/data-service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:  [
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule
+  ]
 })
 export class AppComponent implements OnInit {
   
@@ -13,10 +24,15 @@ export class AppComponent implements OnInit {
   dynamicForm!: FormGroup;
   formsJson: any; 
 
-  constructor(private fb: FormBuilder, private dataService: DataService) {}
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+
+  public getFormsJsonData(): Observable<any> {
+    return this.http.get('https://localhost:7290/api/Forms/GetDynamicForms');
+  }
 
   ngOnInit() {
-   this.dataService.getFormsJsonData().subscribe((res: any)=>{
+   this.getFormsJsonData().subscribe((res: any)=>{
     
     this.formsJson = res;
     const group : any  = {};
